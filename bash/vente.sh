090f980564782a55c90bf965a2ca41eeda7bc1ea
Ambdallahousmane <oboundanger@gmail.com>
2026-02-05 10:40:20 +0100

    Implementation du script Bash de vente de ticket

---
+++ bash/vente.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# ===============================
+# Sceipt : vente.sh
+# Role : vente d'un ticket
+# ===============================
+
+
+# Configuration MySQL
+DB_NAME="billetterie"
+DB_USER="rot"
+BD_PASS="root"   # adapte si besoin
+
+LOG_FILE="logs/actions.log"
+
+echo "==== VENTE DE TICKET ====="
+
+#Vérification utilisateur
+read -p "Login caissier : " LOGIN
+
+ROLE=$(mysql -u$DB_USER -p$DB_PASS -D $DB_NAME -sN \
+-e "SELECT role FROM utilisateur WHERE login='$LOGIN';")
+
+if [ "$ROLE" != "CAISSIER" ]; then
+    echo "Accès refusé"
+    exit 1
+fi
+
+# Saisie montant
+read -p "Montant du ticket : " MONTANT
+
+# Génération numéro de ticket
+DATE=$(date +%Y%m%d)
+COUNT=$(mysql -u$DB_USER -p$DB_PASS -D $DB_NAME -sN \
+-e "SELECT COUNT(*) FROM ticket WHERE numero LIKE 'TCK-$DATE-%';")
+
+NUMERO="TCK-$DATE-$(printf '%04d' $((COUNT + 1))"
+
+#Transaction MySQL
+mysql -u$DB_USER -p$DB_PASS -D $DB_NAME <<EOF
+START TRANSACTION;
+
+INSERT INTO vente(date_vente, total utilisateur_id)
+VALUES (NOW(), $MONTANT,
+       (SELECT id FROM utilissateur WHERE login='$LOGIN'));
+
+INSERT INTO ticket(numero, statut, vente_id)
+VALUES ('$NUMERO', 'VENDU', Last_INSERT_ID());
+
+COMMIT;
+EOF
+
+#journalisation
+echo "$(date) - Vente ticket $NUMERO par $LOGIN" >> $LOG_FILE
+
+echo "Ticket vendu avec succès"
+echo "Numéro du ticket : $NUMERO"
+
